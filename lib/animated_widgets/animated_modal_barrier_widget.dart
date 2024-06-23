30be22faf4943dc3ea973e5c11e15d95566733bf
iZenrix <fajrulf960@gmail.com>
2024-06-23 13:11:27 +0700

    update: change widget for animated modal barrier

--- lib/animated_widgets/animated_modal_barrier_widget.dart
+++ lib/animated_widgets/animated_modal_barrier_widget.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class AnimatedModalBarrierWidget extends StatefulWidget {
   const AnimatedModalBarrierWidget({super.key});
 
   @override
-  State<AnimatedModalBarrierWidget> createState() => _AnimatedModalBarrierWidgetState();
+  State<AnimatedModalBarrierWidget> createState() =>
+      _AnimatedModalBarrierWidgetState();
 }
 
-class _AnimatedModalBarrierWidgetState extends State<AnimatedModalBarrierWidget> with SingleTickerProviderStateMixin {
+class _AnimatedModalBarrierWidgetState extends State<AnimatedModalBarrierWidget>
+    with SingleTickerProviderStateMixin {
   bool _isPressed = false;
   late Widget _animatedModalBarrier;
 
@@ -16,47 +19,61 @@ class _AnimatedModalBarrierWidgetState extends State<AnimatedModalBarrierWidget>
 
   @override
   void initState() {
-    ColorTween _colorTween = ColorTween(begin: Colors.transparent, end: Colors.black.withOpacity(0.5));
+    ColorTween colorTween = ColorTween(
+      begin: Colors.transparent,
+      end: Colors.black.withOpacity(0.5),
+    );
 
     _controller = AnimationController(
-      duration: const Duration(seconds: 1),
+      duration: const Duration(seconds: 3),
       vsync: this,
     );
 
-    _colorAnimation = _colorTween.animate(_controller)
-      ..addListener(() {
-        setState(() {});
-      });
+    _colorAnimation = colorTween.animate(_controller);
 
     _animatedModalBarrier = AnimatedModalBarrier(
       color: _colorAnimation,
-      dismissible: false,
-      semanticsLabel: 'Semantics Label',
+      dismissible: true,
     );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            if (_isPressed) {
-              _controller.reverse();
-            } else {
-              _controller.forward();
-            }
-            _isPressed = !_isPressed;
-          },
-          child: Text(_isPressed ? 'Hide Modal Barrier' : 'Show Modal Barrier'),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              width: 200,
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _isPressed = true;
+                      });
+                      _controller.reset();
+                      _controller.forward();
+                      Future.delayed(const Duration(seconds: 5), () {
+                        setState(() {
+                          _isPressed = false;
+                        });
+                      });
+                    },
+                    child: const Text('Press Me'),
+                  ),
+                  if (_isPressed) _animatedModalBarrier,
+                ],
+              ),
+            ),
+          ],
         ),
-        Expanded(
-          child: Center(
-            child: _animatedModalBarrier,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
